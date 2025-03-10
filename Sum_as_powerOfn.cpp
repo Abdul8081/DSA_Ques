f33485eee40acccfd2fa054a6e81752ee8bd8ef9
Abdul8081 <amuid677@gmail.com>
2025-03-10 07:45:01 +0530

    If number can be expressed as sum of distinct power of k. k>=2

--- Sum_as_powerOfn.cpp
+++ Sum_as_powerOfn.cpp
@@ -1,14 +1,12 @@
-#include<bits/stdc++.h>
-using namespace std;
 class Solution {
     public:
         
         bool checkPowersOfThree(int n) {
-            //similarly for the number,if there is other than 3, we can also do here for 4, 5, 6, 7, 
-            //idea is : if cofficient is coming > 1 then it will not correct, as we have to take distinct powers, and if cofficint is coming other than 0,1 than it means that power has been added by more than 1. 
+            //here for any k, we can do this. 
+            int k = 3; 
             while(n) {
-                if(n%3==2) return false;
-                n/=3; 
+                if(n%k>1) return false;
+                n/=k; 
             }
             return true;
         }
