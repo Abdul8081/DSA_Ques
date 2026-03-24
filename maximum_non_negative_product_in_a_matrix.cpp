4e63910336d637f849ac6f0f11aea2c8eddcb87f
Abdul8081 <amuid677@gmail.com>
2026-03-24 23:08:10 +0530

    leet(1594): added buttom up solution as well for the same problem

--- maximum_non_negative_product_in_a_matrix.cpp
+++ maximum_non_negative_product_in_a_matrix.cpp
@@ -56,3 +56,56 @@ public:
         return ansMax < 0 ? -1 : ansMax % M;
     }
 };
+
+
+//Approach 2
+
+class Solution {
+public:
+    int M = 1e9 + 7;
+    typedef long long int ll;
+    typedef pair<ll, ll> pp;
+    int m; 
+    int n;
+
+    int maxProductPath(vector<vector<int>>& grid) {
+        m = grid.size();
+        n = grid[0].size();
+
+        vector<vector<pp>> p(m, vector<pp> (n)); // {maxVal, minVal}
+
+        // filling in the first row
+        p[0][0] = {grid[0][0], grid[0][0]};
+        for(int c = 1; c < n; c++) {
+            p[0][c].first = p[0][c-1].first * grid[0][c]; 
+            p[0][c].second = p[0][c-1].second * grid[0][c];
+        }
+
+        // filling int the fist col
+        for(int r = 1; r < m; r++) {
+            p[r][0].first = p[r-1][0].first * grid[r][0];
+            p[r][0].second = p[r-1][0].second * grid[r][0];
+        }
+
+        //filling in the internal cell
+
+        for(int r = 1; r < m; r++) {
+            for(int c = 1; c < n; c++) {
+                ll el = grid[r][c];
+
+                ll leftMax = p[r][c-1].first;
+                ll leftMin = p[r][c-1].second;
+
+                ll topMax = p[r-1][c].first;
+                ll topMin = p[r-1][c].second;
+
+                p[r][c].first  = max({el * leftMax, el* topMax, el * leftMin, el* topMin});
+                p[r][c].second = min({el * leftMax, el* topMax, el * leftMin, el* topMin});
+            }
+        }
+
+        auto [maxVal, _] = p[m-1][n-1];
+        return maxVal < 0 ? -1 : maxVal % M;
+        
+    }
+};
